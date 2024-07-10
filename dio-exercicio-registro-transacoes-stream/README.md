0a3548cb1752b4d19846b3873997fa5f9a21d2e7
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-09 22:38:12 -0300

    Exercício: Registro de Transações Bancárias com Stream API

---
+++ dio-exercicio-registro-transacoes-stream/README.md
@@ -0,0 +1,11 @@
+# Exercício: Registro de Transações Bancárias com Stream API
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=CONCLUIDO&color=dark&style=for-the-badge)
+
+## Tecnologias Utilizadas
+<div style="display: inline_block">
+	<img align="center" alt="Matheusxr77-Java" height="50" width="50" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg">
+	<img align="center" alt="Matheusxr77-VsCode" height="50" width="50" src="https://github.com/devicons/devicon/blob/master/icons/vscode/vscode-original.svg">
+</div>
+
+## Descrição
+Você está aprimorando o programa anterior e agora deseja utilizar a Stream API do Java para processar as transações bancárias de forma mais eficiente.
---
+++ dio-exercicio-registro-transacoes-stream/src/RegistroTransacoesComStream.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import model.Transacao;
+
+public class RegistroTransacoesComStream {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<Transacao> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+          
+            char tipoTransacao = scanner.next().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            Transacao transacao = new Transacao(tipoTransacao, valorTransacao);
+            transacoes.add(transacao);
+
+            if (Character.toUpperCase(transacao.getTipo()) == 'D') {
+                saldo += valorTransacao;
+            } else if (Character.toUpperCase(transacao.getTipo()) == 'S') {
+                saldo -= valorTransacao;
+            }
+        }
+
+        System.out.println("\nSaldo : " + saldo);
+        System.out.println("\nTransacoes:");
+        transacoes.stream()
+                .map(transacao -> transacao.getTipo() + " de " + transacao.getValor())
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        scanner.close();
+    }
+
+}
